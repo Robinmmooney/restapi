a54b540dc41fe3b5967bed216a332467b1b869c0
Robin Mooney <robinmooney.student@careerdevs.com>
2019-01-05 17:40:15 +0000

    updated the index.js file to include a get and respond to run the web page and and return the name Yoshi

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 
-//set up express app
+//setup express app
+
 const app = express();
+
+app.get('/api',function(req, res){
+  console.log('GET request');
+  res.send({name:'Yoshi'});
+});
 
 //listens for requests
 
-app.listen(process.env.port||4000,function(){
-    console.log('now listening for requests');
+app.listen(process.env.PORT || 4000, function(){
+  console.log('now listening for requests');
 });
-
 
 
 
@@ -19,6 +24,10 @@ app.listen(process.env.port||4000,function(){
 
 // // set up express app
 // const app = express();
+
+// app.get('/api', function(req, res){
+//   res.send({ name: 'Yoshi' });
+// });
 
 // // listen for requests
 // app.listen(process.env.port || 4000, function(){
