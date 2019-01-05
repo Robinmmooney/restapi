729ff951a63ad8b0a047e9da0a318379249b3be2
Robin Mooney <robinmooney.student@careerdevs.com>
2019-01-05 16:53:51 +0000

    setup an initial express app to listen to port 4000

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+//set up express app
+const app = express();
+
+//listens for requests
+
+app.listen(process.env.port||4000,function(){
+    console.log('now listening for requests');
+});
+
+
+
+
+
+
+
+// const express = require('express');
+
+// // set up express app
+// const app = express();
+
+// // listen for requests
+// app.listen(process.env.port || 4000, function(){
+//     console.log('now listening for requests');
+// });
