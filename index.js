783808e5eb5193a21146a7b0275c6d62eb946242
Robin Mooney <robinmooney.student@careerdevs.com>
2019-01-09 19:23:26 +0000

    used postman to send a name and rank testing out the index and api js files

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
 const express = require('express');
-//const bodyParser
+const bodyParser = require('body-parser');
 
-//setup express
+// set up express app
 const app = express();
 
-//initialize routes
+// use body-parser middleware
+app.use(bodyParser.json());
+
+// initialize routes
 app.use('/api', require('./routes/api'));
 
-//listens for requests
+// listen for requests
 app.listen(process.env.PORT || 4000, function(){
-   console.log('now listening for requests') ;
+    console.log('now listening for requests');
 });
-
-
-
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// // set up express app
-// const app = express();
-
-// // use body-parser middleware
-// app.use(bodyParser.json());
-
-// // initialize routes
-// app.use('/api', require('./routes/api'));
-
-// // listen for requests
-// app.listen(process.env.port || 4000, function(){
-//     console.log('now listening for requests');
-// });
